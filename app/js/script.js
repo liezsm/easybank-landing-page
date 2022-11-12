bb7b030f8d711f94206f0ecc762b7d6d320ecf2f
liezsm <liez.dev@gmail.com>
2022-11-12 12:21:42 +0800

    initialize project

---
+++ app/js/script.js
@@ -0,0 +1,8 @@
+const testString = "This is a test!";
+
+
+const fruits = ["mango", "papaya", "lychee"];
+
+fruits.map((fruit) => {
+  console.log(fruit);
+});
---
+++ dist/script.js
@@ -0,0 +1,2 @@
+const testString="This is a test!",fruits=["mango","papaya","lychee"];fruits.map((s=>{console.log(s)}));
+//# sourceMappingURL=script.js.map
